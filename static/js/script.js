e162f30e71610ede840336adce3ed5ece2997868
timothytylim <timothylim@berkeley.edu>
2017-10-16 01:52:00 +0000

    Have to do mailgun plus learning.md

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,71 @@
-// script.js
+function validateForm() {
+	event.preventDefault();
+    var x = document.forms['form']["dname"].value;
+    var y = document.forms['form']["dsubject"].value;
+    var z = document.forms['form']["dmessage"].value;
+    if (x == "" || y == "" || z == "") {
+        document.getElementById("mess").innerHTML = "Do not leave any fields empty! All required.";
+        return false;
+    } else {
+    	document.getElementById("mess").innerHTML = "Submitted!!!";
+    }
+}
 
-console.log("js/script.js has been loaded!");
+var x = document.getElementById("form");
+var createform = document.createElement('form'); 
+createform.setAttribute("name", "form")
+createform.setAttribute("action", "");
+createform.setAttribute("method", "post");
+x.appendChild(createform);
+
+var heading = document.createElement('h2');
+heading.innerHTML = "Contact Us!";
+createform.appendChild(heading);
+
+var line = document.createElement('hr'); 
+createform.appendChild(line);
+
+var linebreak = document.createElement('br');
+createform.appendChild(linebreak);
+
+var namelabel = document.createElement('label');
+namelabel.innerHTML = "Your Name : "; 
+createform.appendChild(namelabel);
+
+var inputelement = document.createElement('input');
+inputelement.setAttribute("type", "text");
+inputelement.setAttribute("name", "dname");
+createform.appendChild(inputelement);
+
+var linebreak = document.createElement('br');
+createform.appendChild(linebreak);
+
+var sublabel = document.createElement('label');
+sublabel.innerHTML = "Subject : ";
+createform.appendChild(sublabel);
+
+var subelement = document.createElement('input');
+subelement.setAttribute("type", "text");
+subelement.setAttribute("name", "dsubject");
+createform.appendChild(subelement);
+
+var subbreak = document.createElement('br');
+createform.appendChild(subbreak);
+
+var messagelabel = document.createElement('label'); 
+messagelabel.innerHTML = "Your Message : ";
+createform.appendChild(messagelabel);
+
+var texareaelement = document.createElement('textarea');
+texareaelement.setAttribute("name", "dmessage");
+createform.appendChild(texareaelement);
+
+var messagebreak = document.createElement('br');
+createform.appendChild(messagebreak);
+
+var submitelement = document.createElement('input'); 
+submitelement.setAttribute("type", "submit");
+submitelement.setAttribute("name", "dsubmit");
+submitelement.setAttribute("value", "Submit");
+submitelement.setAttribute("onclick", "validateForm()");
+createform.appendChild(submitelement);
--- webserver.py
+++ webserver.py
@@ -4,16 +4,26 @@ webserver.py
 File that is the central location of code for your webserver.
 """
 
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, url_for,request
+from os import environ
+import json
+import requests
 # Create application, and point static path (where static resources like images, css, and js files are stored) to the
 # "static folder"
 app = Flask(__name__,static_url_path="/static")
 
-@app.route('/')
-def hello_world():
-    """
-    If someone goes to the root of your website (i.e. http://localhost:5000/), run this function. The string that this
-    returns will be sent to the browser
-    """
-    return render_template("index.html") # Render the template located in "templates/index.html"
+@app.route('/index')
+def to_index():
+	return render_template('index.html')
+
+@app.route('/about')
+def to_about():
+	return render_template('about.html')
+
+@app.route('/contact')
+def to_contact():
+	return render_template('contact.html')
+
+@app.route('/blog/<article>')
+def render_article(article=None):
+	return render_template(article + '.html')
